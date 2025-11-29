4224f391f4591df4efa40910b819870a48a99faf
Erwin Saul <mackonkey_2@hotmail.com>
2025-11-29 13:55:49 -0400

    Avanzando el Evaluador

--- src/Puriq/Evaluator.hs
+++ src/Puriq/Evaluator.hs
@@ -24,3 +24,43 @@ evaluar (ExpUnaria op expr) = do
 evaluar (ExpVariable nombre) = do
   Left (ErrorVariableNoDefinida nombre)
 
+-- Operacion Suma
+aplicarOperadorBinario :: String -> Valor -> Valor -> Either EvalError Valor
+aplicarOperadorBinario "+" (Entero a) (Entero b) = Right (Entero (a+b))
+aplicarOperadorBinario "+" (Decimal a) (Decimal b) = Right (Decimal (a+b))
+aplicarOperadorBinario "+" (Entero a) (Decimal b) = Right (Decimal (fromIntegral a + b))
+aplicarOperadorBinario "+" (Decimal a) (Entero b) = Right (Decimal (a + fromIntegral b))
+
+-- Operacion Resta
+aplicarOperadorBinario "-" (Entero a) (Entero b) = Right (Entero (a-b))
+aplicarOperadorBinario "-" (Decimal a) (Decimal b) = Right (Decimal (a-b))
+aplicarOperadorBinario "-" (Entero a) (Decimal b) = Right (Decimal (fromIntegral a - b))
+aplicarOperadorBinario "-" (Decimal a) (Entero b) = Right (Decimal (a-fromIntegral b))
+
+-- Operación Multiplicación 
+aplicarOperadorBinario "*" (Entero a) (Entero b) = Right (Entero (a*b))
+aplicarOperadorBinario "*" (Decimal a) (Decimal b) = Right (Decimal (a*b))
+aplicarOperadorBinario "*" (Entero a) (Decimal b) = Right (Decimal (fromIntegral a * b))
+aplicarOperadorBinario "*" (Decimal a) (Entero b) = Right (Decimal (a*fromIntegral b))
+
+-- Operación División
+aplicarOperadorBinario "/" _ (Entero 0) = Left ErrorDivisionPorCero
+aplicarOperadorBinario "/" _ (Decimal 0.0) = Left ErrorDivisionPorCero
+aplicarOperadorBinario "/" (Entero a) (Entero b) = Right (Decimal (fromIntegral a / fromIntegral b))
+aplicarOperadorBinario "/" (Decimal a) (Decimal b) = Right (Decimal (a/b))
+aplicarOperadorBinario "/" (Entero a) (Decimal b) = Right (Decimal (fromIntegral a / b))
+aplicarOperadorBinario "/" (Decimal a) (Entero b) = Right (Decimal (a / fromIntegral b))
+
+-- En caso de que el operador no sea reconocido
+aplicacionOperadorBinario _ _ = Left (ErrorOperadorDesconocido op)
+
+aplicarOperadorUnario :: String -> Valor -> Either EvalError Valor
+aplicarOperadorUnario "-" (Entero n) = Right (Entero (-n))
+aplicarOperadorUnario "-" (Decimal n) = Right (Decimal (-n))
+aplicarOperadorUnario op _ = Left (ErrorOperadorDesconocido op)
+
+
+
+
+
+
--- test/Puriq/EvaluatorSpec.hs
+++ test/Puriq/EvaluatorSpec.hs
@@ -1,18 +1,31 @@
 module Puriq.EvaluatorSpec (spec) where
 
 import Test.Hspec
+import Puriq.Types
+import Puriq.Evaluator
+import Puriq.Parser (parseFromString)
 --import Test.QuickCheck
 --import Puriq.Types
 
 spec :: Spec
 spec = do
-  describe "Evaluator" $ do
-    describe "evaluación básica" $ do
-      it "evalúa literales enteros" $ do
-        --True `shouldBe` True
-        pending
-        
-      it "evalúa operciones aritméticas" $ do
-        --True `shouldBe` True
-        pending
+  describe "Evaluator de expresiones matemáticas" $ do
+
+    describe "literales" $ do
+      it "evalúa un entero literal" $ do
+        case parseFromString "42" of
+          Left err -> expectationFailure $ "Error de parsing: "++ show err
+          Right expr -> evaluar expr `shouldBe` Right (Entero 42)
+      
+      it "evalúa un decimal literal" $ do
+        case parseFromString "3.14" of
+          Left err -> expectationFailure $ "Error de parsing: " ++ show err
+          Right expr -> evaluar expr `shouldBe` Right (Decimal 3.14)
+    
+    describe "suma" $ do
+      it "suma dos enteros" do
+        case parseFromString "2 + 3" of
+          Left err -> expectationFailure $ "Error de parsing: " ++ show err
+          Right expr -> evaluar expr `shouldBe` Right (Entero 5)
+    
       
